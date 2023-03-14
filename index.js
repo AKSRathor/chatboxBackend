3f173c963810e3a57bcf01036c73adf1dc1dbaf0
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2023-03-14 11:39:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "chatbox.vercel.app",
+    origin: "https://webchatbox.vercel.app",
     methods: ["GET", "POST"],
     transports: ['websocket']
   }
